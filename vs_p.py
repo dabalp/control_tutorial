f2ddfe1e877e42ee919a93bbbdc7cfa9a91c11f8
jhird <jhird@bristol.ac.uk>
2019-10-11 12:15:44 +0100

    add integral term

--- vs_p.py
+++ vs_p.py
@@ -3,6 +3,8 @@ c = FgClient()
 c.ap_pitch_off()
 
 kk = 0
+error = 0
+err_sum = 0
 while True:
   kk+=1
   c.tic()
@@ -11,6 +13,8 @@ while True:
   else:
     vs_des = 0.0
   vs = c.vertical_speed_fps()
-  c.set_elevator(-0.01*(vs_des - vs))
+  error = vs_des - vs
+  err_sum += error
+  c.set_elevator(-0.03*(error) - 0.005*err_sum)
   print(vs)
   c.toc(0.5)
